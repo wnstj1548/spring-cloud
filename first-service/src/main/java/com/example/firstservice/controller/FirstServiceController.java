b89dae98f1c865cdcb33423bfacb1bb8c789bb6f
wnstj1548 <wnstj0614@naver.com>
2025-04-03 00:05:49 +0900

    feat : "GlobalFilter / eureka연결 및 load balancer"

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,6 +1,9 @@
 package com.example.firstservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +29,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest req) {
+        log.info("Server port={}", req.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on %s", env.getProperty("local.server.port"));
     }
 }
